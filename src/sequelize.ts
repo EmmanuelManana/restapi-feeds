f27d09002f9abacfb69fb68083ddbd244cbfa075
EmmanuelManana <emanana@student.wethinkcode.co.za>
2020-12-25 19:40:27 +0200

    remove console logs

--- src/sequelize.ts
+++ src/sequelize.ts
@@ -5,10 +5,7 @@ import { config } from './config/config';
 const c = config.dev;
 
 // Instantiate new Sequelize instance!
-console.log("config username - " + c.username);
-console.log("config password - " + c.password);
-console.log("config database - " + c.database);
-console.log("config host - " + c.host);
+
 export const sequelize = new Sequelize({
   "username": c.username,
   "password": c.password,
